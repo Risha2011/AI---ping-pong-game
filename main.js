ba3f4613245c9f54850a8d897a5f461ed5aef0ca
Risha2011 <74859480+Risha2011@users.noreply.github.com>
2021-04-26 15:07:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,7 +22,21 @@ var ball = {
 }
 
 function setup(){
-  var canvas =  createCanvas(700,600);
+  var canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	
+	video = createCapture(VIDEO);
+	video.size(800,400);
+  video.hide();
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
 }
 
 
